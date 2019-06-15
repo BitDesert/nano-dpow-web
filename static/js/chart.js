af9b8aa15cc74c208ad5f942878fd4943a13bd6f
Tobias Palmer <tobiaspalmer93@gmail.com>
2019-06-15 14:15:16 +0200

    working last work chart

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,42 +1,32 @@
-var ctx = document.getElementById('chart');
-
 var work_timing = [];
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+var lastWorkChart = new Chart(document.getElementById('lastWorkChart'), {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Time in ms',
+      data: [],
+      backgroundColor: '#f44336',
+      borderColor: '#f44336',
+      borderWidth: 1,
+      fill: false
+    }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Time in ms'
+          }
         }
+      }]
     }
+  }
 });
 
 client.on("message", function (topic, payload) {
@@ -53,20 +43,34 @@ client.on("message", function (topic, payload) {
     var work_type = topic_split[1]
 
     console.log('work', work_type, block_hash, difficulty)
-      console.log('Got work, adding to work cache...');
-      work_cache.push({
-        block_hash: block_hash,
-        difficulty: difficulty,
-        work_type: work_type,
-        startTime: new Date()
-      })
+    console.log('Got work, adding to work timing...');
+    work_timing.push({
+      block_hash: block_hash,
+      difficulty: difficulty,
+      work_type: work_type,
+      startTime: new Date()
+    })
 
   } else if (message_type == 'cancel') {
     console.log('cancel', topic_split, payload)
 
-    if (work_cache.some(e => e.block_hash === payload)) {
-      console.log('In work cache, removing')
-      work_cache = work_cache.filter(e => e.block_hash !== payload);
+    if (work_timing.some(e => e.block_hash === payload)) {
+
+      var datediff = new Date() - work_timing.filter(e => e.block_hash === payload)[0].startTime;
+
+      console.log('Work is done for ' + payload + ' in ' + datediff + ' ms')
+
+      if (lastWorkChart.data.labels.length > 25) {
+        lastWorkChart.data.labels.shift();
+        lastWorkChart.data.datasets[0].data.shift();
+      }
+
+      lastWorkChart.data.labels.push(payload.slice(-4))
+      lastWorkChart.data.datasets[0].data.push(datediff)
+
+      lastWorkChart.update();
+
+      work_timing = work_timing.filter(e => e.block_hash !== payload);
     }
 
   }
